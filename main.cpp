92008f4eaaafaf59b326fcabf9a1a360865fd721
phoenix16 <c.prakriti@gmail.com>
2013-06-17 22:12:04 -0700

    added comments

--- main.cpp
+++ main.cpp
@@ -1,32 +1,39 @@
-#include "MotionEstimator.h"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
 
-#define PI 3.1415926535;
+#define MAX_FEATURES 400
+
+using namespace cv;
+using namespace std;
+
 #define NUM_FRAMES_TO_PROCESS  600
 
-
+// Global Variables
 Mat frame1, img_object;
 Point pt1, pt2;
 bool roi_capture = false;
 bool got_roi = false;
 
-
+// Function Headers
 void mouse_click(int event, int x, int y, int flags, void *param);
-
+void findObject(Mat& frame, Mat& img_object);
 
 int main(int argc, char* argv[])
 {
     if (argc != 2)
     {
-        cout << "Usage: ./ObjectTracker <input video file>" << endl;
+        cout << "Usage: ./ObjectTracker <Input Video file>" << endl;
         return -1;
     }
 
     VideoCapture inputVideo(argv[1]);
     if (!inputVideo.isOpened())
     {
-        cout << "Error opening input video " << argv[1] << endl;
+        cout << "Error opening Input Video " << argv[1] << endl;
         return -1;
     }
+
     double fps = inputVideo.get(CV_CAP_PROP_FPS);
     int delay = 1000/fps;    // time in ms between successive frames = 1000/fps
     cout << "FPS = " << fps << endl;
@@ -42,16 +49,12 @@ int main(int argc, char* argv[])
         imshow("Frame 1: Capture ROI", frame1);
         setMouseCallback("Frame 1: Capture ROI", mouse_click, 0);
 
-                waitKey(0);
+        waitKey(0);
 
         // close the window after ROI is saved
         destroyWindow("Frame 1: Capture ROI");
         destroyWindow("ROI");
     }
-
-
-    // Create instance of Motion Estimator
-    MotionEstimator m;
 
     Mat frame;
     bool stop(false);
@@ -63,18 +66,18 @@ int main(int argc, char* argv[])
     // Process every frame of video only when object ROI has been selected
     if (!img_object.empty())
     {
-        namedWindow("input video");
-        moveWindow("input video", 600, 300);
+        namedWindow("Input Video");
+        moveWindow("Input Video", 600, 300);
 
         while(!stop)
         {
             inputVideo >> frame;            // read current frame
             if( frame.empty()) break;       // check if at end
             frameNum++;
-            imshow("input video", frame);
-            cout << "\nFrame Number : " << frameNum << endl;
+            imshow("Input Video", frame);
 
-            //m.performFeatureDetection(frame);
+            // Find the object in current frame, draw a rectangle around it
+            findObject(frame, img_object);
 
             if (inputVideo.get(CV_CAP_PROP_POS_FRAMES) == NUM_FRAMES_TO_PROCESS)
             {
@@ -92,8 +95,8 @@ int main(int argc, char* argv[])
 }
 
 
-//Callback for mousclick event, the x-y coordinate of mouse button-up and button-down
-//are stored in two points pt1, pt2.
+// Records mouseclick events to get x-y coordinates of ROI
+// Store coordinates in global variables pt1, pt2.
 void mouse_click(int event, int x, int y, int flags, void *param)
 {
     switch(event)
@@ -136,4 +139,42 @@ void mouse_click(int event, int x, int y, int flags, void *param)
         break;
     }
     }
+}
+
+// Function to perform template matching and track the object in the video
+void findObject(Mat& frame, Mat& img_object)
+{
+    // Source image to display
+    Mat img_display;
+    frame.copyTo(img_display);
+
+    // Create the result matrix
+    Mat result;
+    int result_cols =  frame.cols - img_object.cols + 1;
+    int result_rows = frame.rows - img_object.rows + 1;
+    result.create(result_cols, result_rows, CV_32FC1);
+
+    // Do the Template Matching and Normalize
+    // Matching method choices are:
+    // CV_TM_SQDIFF, CV_TM_SQDIFF_NORMED, CV_TM_CCORR, CV_TM_CCORR_NORMED, CV_TM_CCOEFF, CV_TM_CCOEFF_NORMED
+    int match_method = CV_TM_SQDIFF_NORMED;
+    matchTemplate( frame, img_object, result, match_method );
+    normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() );
+
+    // Localizing the best match with minMaxLoc
+    double minVal; double maxVal;
+    Point minLoc; Point maxLoc; Point matchLoc;
+    minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+
+    /// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
+    if( match_method  == TM_SQDIFF || match_method == TM_SQDIFF_NORMED )
+    { matchLoc = minLoc; }
+    else
+    { matchLoc = maxLoc; }
+
+    // Draw a rectangle around the found object
+    rectangle(img_display, matchLoc, Point(matchLoc.x + img_object.cols , matchLoc.y + img_object.rows), Scalar(255,0,0), 2, 8, 0 );
+    namedWindow("Object Tracking", 1);
+    moveWindow("Object Tracking", 1000, 300);
+    imshow("Object Tracking", img_display);
 }
